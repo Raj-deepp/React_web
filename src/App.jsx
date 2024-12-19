67a197664927a18bfffea4a80244a93d23dd87c1
Rajdeep Mandal <mrajdeep651@gmail.com>
2024-12-20 01:34:38 +0530

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,8 @@ import Header from "./components/Header";
 const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        <Button className="mt-10" href="#login">
-          Something is Cooking
-        </Button>
+        <Header />
       </div>
       <ButtonGradient />
     </>
