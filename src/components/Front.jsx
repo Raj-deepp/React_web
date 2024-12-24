7da14160b7029e3f2e87c797d810bd6e542508e4
Rajdeep Mandal <mrajdeep651@gmail.com>
2024-12-25 00:33:51 +0530

    Bg and image

--- src/components/Front.jsx
+++ src/components/Front.jsx
@@ -26,19 +26,28 @@ const Front = () => {
           </Button>
         </div>
         <div className="relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-4">
-          <div className="relative z-1 p-0.5 rounded-2xl bg-conic-gradient">
+          <div className="relative z-1 p-0.5 rounded-2xl bg-conic-gradient ">
             <div className="relative bg-n-8 rounded-[1rem]">
-              <div className="h-[1.4rem] bg-n-10 rounded-t-[0.7rem]" />
-              <div>
+              <div className="h-[1.5rem] bg-n-10 rounded-t-[0.7rem]" />
+              <div className="aspect-[40/30] rounded-b-[0.9rem] overflow-hidden md:aspect-[688/490] lg:aspect-[1024/490]">
                 <img
                   src={robot}
                   className="w-full scale-[1] translate-y-[-3%] md:scale-[1] md:-translate-y-[1%] lg:-translate-y-[1%]"
                   width={1024}
                   height={490}
-                  alt="AI"
+                  alt="</K>"
                 />
               </div>
             </div>
+          </div>
+          <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
+            <img
+              src={heroBackground}
+              className="w-full"
+              width={1440}
+              height={1800}
+              alt="hero"
+            />
           </div>
         </div>
       </div>
